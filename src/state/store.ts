5d2b797ee9db6558448f9abefd1accd4dcdc57ec
Milad Asghari Amlashi <miladasghari2016@gmail.com>
2022-08-31 08:58:09 +0300

    Add redux state sync

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,18 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import basketReducer from "./basketSlice";
+import {
+  createStateSyncMiddleware,
+  initMessageListener,
+} from "redux-state-sync";
+const reduxStateSyncConfig = {};
 
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
   },
+  middleware: [createStateSyncMiddleware(reduxStateSyncConfig)],
 });
+initMessageListener(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
